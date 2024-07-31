3f145ff4a967865d76a0113e33add995954bc627
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-07-31 16:30:43 -0400

    ADD Router navigation on component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,24 +3,26 @@ import { AccountService } from './../_services/account.service';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink],
+  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService) // I use the inject to inject the service because I need it in the constructor.
+  private router = inject(Router)
+
   model: any = {} // This model apper in the form .html and i pass with the input data
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: () => {
+        void this.router.navigateByUrl('/members'); // In this case we have a alert in the Angular Server, this is because we need to use void when we don't have a return
       },
       error: error => console.log(error)
     })
@@ -28,6 +30,7 @@ export class NavComponent {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
