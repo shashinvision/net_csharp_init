dd520e76db4cf9e9a783d90342d1ef50b39202f7
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-07-31 15:07:20 -0400

    ADD input on props

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,27 @@
 import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { User } from './_models/User';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent],
+  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
 
-  http = inject(HttpClient);
   private AccountService = inject(AccountService);
   title = 'DatingApp';
-  users: any;
+
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-    
+
   }
 
   setCurrentUser() {
@@ -34,11 +32,5 @@ export class AppComponent implements OnInit{
     this.AccountService.currentUser.set(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: reponse =>  this.users = reponse,
-      error: error => console.log(error),
-      complete: ()=> console.log('Request has completed')
-    });
-  }
+
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  // @Input() usersFormHomeComponent: any; // All of this wa can be used
+  usersFormHomeComponent = input.required<any>()
+  model: any = {}
+
+  register(){
+    console.log(this.model)
+  }
+
+  cancel(){
+    console.log('cancelled')
+  }
+}
