d2b6451231c6e72f0291004a2c52d1372b7e978b
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-07-31 09:57:52 -0400

    ADD Login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { AccountService } from './../_services/account.service';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService) // I use the inject to inject the service because I need it in the constructor.
+  loggedIn = false;
+  model: any = {}
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout(){
+    this.loggedIn = false;
+  }
 
 }
