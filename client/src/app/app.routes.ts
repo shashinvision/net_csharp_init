babace9c7e4fdd7adebbf0ba17cf205e26d92efc
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-08-01 11:10:55 -0400

    ADD test errors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -23,5 +24,6 @@ export const routes: Routes = [
   // {path: 'member/:id', component: MemberDetailComponent},
   // {path: 'lists', component: ListsComponent},
   // {path: 'messages', component: MessagesComponent},
+  {path: 'errors', component: TestErrorsComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
