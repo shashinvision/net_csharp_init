07bd59cc122867b11f8503373f12c4e8511d85dc
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-08-06 17:37:07 -0400

    ADD member detail

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -17,7 +17,7 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       {path: 'members', component: MemberListComponent, canActivate: [authGuard]},
-      {path: 'member/:id', component: MemberDetailComponent},
+      {path: 'member/:username', component: MemberDetailComponent},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
     ]
