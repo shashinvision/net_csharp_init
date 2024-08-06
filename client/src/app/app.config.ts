146391b1ac2a5ecbb85b4a7a99e1989c18fa2b93
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-08-06 16:45:28 -0400

    ADD Interceptor for JWT

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,12 +6,13 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
   provideRouter(routes),
-  provideHttpClient(withInterceptors([errorInterceptor])), // using own interceptor
+  provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])), // using own interceptor
   provideAnimations(), // To use dropdown menu with animations globally
   provideToastr({
     positionClass: 'toast-bottom-right',
