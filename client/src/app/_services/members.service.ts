6cb299fc955ef3750be58bece900a8d07b3b2ee8
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-08-07 12:16:44 -0400

    ADD Update member services

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/Member';
+import { of, tap } from 'rxjs';
 // import { AccountService } from './account.service';
 
 @Injectable({
@@ -12,20 +13,32 @@ export class MembersService {
   private http = inject(HttpClient);
   // private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([])
 
   getMembers() {
     // return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: members => this.members.set(members)
+    });
   }
 
   getMember(username: string) {
+    const member = this.members().find(x => x.username === username)
+    if(member !== undefined) return of(member);
     // return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
     // return this.http.put(this.baseUrl + 'users', member, this.getHttpOptions());
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if(m.username === member.username) return member;
+          return m
+        }))
+      })
+    );
   }
 
   // getHttpOptions() {
