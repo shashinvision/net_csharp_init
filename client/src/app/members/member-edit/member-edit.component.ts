a9b7875a4fc3dce766b7e0f1b2b831f7b1e95bf5
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-08-07 08:58:39 -0400

    ADD Edit profile

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Member } from '../../_models/Member';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
   standalone: true,
-  imports: [],
+  imports: [TabsModule, FormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
 })
-export class MemberEditComponent {
+export class MemberEditComponent implements OnInit {
+  member?: Member;
+
+  private accountService = inject(AccountService);
+  private membersService = inject(MembersService);
+
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+
+    if(!user) return;
+
+    this.membersService.getMember(user.userName).subscribe({
+      next: member => this.member = member
+    })
+
+  }
+
 
 }
